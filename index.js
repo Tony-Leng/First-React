371d3d40992499a18c33dde126afabb13acc9306
Tony Leng <lengtk@gmail.com>
2022-10-18 14:12:40 +0900

    updated index.js with navbar

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
